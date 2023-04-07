85d1163cadddd6010f86e7f6a5d68e082f1e9d04
Kirill Voronin <voroninkv09@gmail.com>
2023-04-07 08:48:14 +0300

    fixed bug in PageTitle.js

--- src/components/Statistics/PageTitle/PageTitle.js
+++ src/components/Statistics/PageTitle/PageTitle.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from 'components/Statistics/PageTitle/PageTitle.module.css';
 
 export function PageTitle({ text }) {
-  return text ? <h2 className={css.title}>{text}</h2> : '';
+  return text && <h2 className={css.title}>{text}</h2>;
 }
 PageTitle.propTypes = {
   text: PropTypes.string,
